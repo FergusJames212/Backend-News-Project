b5bbd70f591a3f4f3406f7e12f651cf4d126b52a
FergusJames212 <fergusarjames@virginmedia.com>
2023-02-22 09:01:22 +0000

    removed a console log

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -35,7 +35,6 @@ exports.getCommentsByArticleId = (req, res, next) => {
     const article_id = req.params.article_id;
     fetchCommentsByArticleId(article_id)
     .then((comments) => {
-        console.log(comments, "comments")
         res.status(200).send({ comments })
     })
     .catch((err) => {
